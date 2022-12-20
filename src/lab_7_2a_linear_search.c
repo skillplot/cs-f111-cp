019f42669538630ca705aaab95a03b65845b0b23
Nikhil Handa <77016507+infinitecoder1729@users.noreply.github.com>
2022-12-20 15:04:22 +0530

    Create lab_7_2a_linear_search.c
    LabSheet 7 
    Q2a - linearSearch()

---
+++ src/lab_7_2a_linear_search.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int linearSearch(int array[],int search)
+{
+  int c;
+  for (c = 0; c < 10; c++)
+  {
+    if (array[c] == search)
+    {
+      return c;
+    }
+  }
+  return -1;
+}
+
+int main(void)
+{
+  int n=10;
+  int search,rval;
+  int array[n];
+  printf("Enter %d integer(s) : ", n);
+  for (int c = 0; c < n; c++)
+    scanf("%d", &array[c]);
+  printf("Enter the number to search : ");
+  scanf("%d", &search);
+  rval=linearSearch(array,search);
+  if (rval==-1)
+  {
+    printf("Element not found\n");
+  }
+  else
+  {
+    printf("Element found at index number : %i\n",rval);
+  }
+  return 0;
+}
